35584d348962e8e8efc25a886234aaa8e656951f
gizmo385 <gizmo385@users.noreply.github.com>
2025-02-01 02:44:09 -0700

    Add some diff specific bindings

--- lazy_github/lib/bindings.py
+++ lazy_github/lib/bindings.py
@@ -96,6 +96,15 @@ class LazyGithubBindings:
     # Workflows
     TRIGGER_WORKFLOW = Binding("T", "trigger_workflow", "Trigger Workflow", show=True, id="workflows.trigger")
 
+    # Diff Viewer
+    DIFF_ADD_COMMENT = Binding("a", "add_comment", "Add Comment", show=True, id="diff.add_comment")
+    DIFF_SELECT_LINE = Binding("V,v", "select_line", "Select Line", show=True, id="diff.select_line")
+    DIFF_CURSOR_DOWN = Binding("j", "cursor_down", show=False, id="diff.cursor_down")
+    DIFF_CURSOR_UP = Binding("k", "cursor_up", show=False, id="diff.cursor_up")
+    DIFF_CLEAR_SELECTION = Binding("escape", "clear_selection", "Clear Selection", show=True, id="diff.clear_selection")
+    DIFF_NEXT_HUNK = Binding("J", "next_hunk", "Next Hunk", show=True, id="diff.next_hunk")
+    DIFF_PREVIOUS_HUNK = Binding("K", "previous_hunk", "Previous Hunk", show=True, id="diff.previous_hunk")
+
     # Notifications
     OPEN_NOTIFICATIONS_MODAL = Binding(
         "ctrl+n",
